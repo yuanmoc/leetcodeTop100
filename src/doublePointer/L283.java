30c89f69283a7ef9c43c6f1df4215d7ce75c7a6f
yuanmoc <gyuanmoc@gmail.com>
2023-08-25 21:13:59 +0800

    leetcode

--- src/doublePointer/L283.java
+++ src/doublePointer/L283.java
@@ -1,8 +1,44 @@
 package doublePointer;
 
+import utils.ArrayUtil;
+import utils.StringUtil;
+
 /**
+ *给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  *
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 示例 2:
+ *
+ * 输入: nums = [0]
+ * 输出: [0]
  */
 public class L283 {
 
+    public static void main(String[] args) {
+        int[] randomArray = {2,1};
+        moveZeroes(randomArray);
+        StringUtil.print(randomArray);
+    }
+
+    public static void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length - i; j++) {
+                // 要求排序时，可以这样判断
+                // nums[j-1] > nums[j] && nums[j] != 0  || nums[j-1] == 0
+                // 题目没有要求排序，所以直接判断0就好
+                if (nums[j-1] == 0) {
+                    int tmp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+    }
 }
